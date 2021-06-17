21619f9d373890e67985179cd1e8c9184e7aba84
Vladyslav92 <vs5956012@gmail.com>
2021-06-18 01:53:38 +0300

    Create class Application and method delete

--- main.py
+++ main.py
@@ -25,3 +25,25 @@ class User(object):
 
     def __str__(self):
         return "{} {:>15} {:>15} {:>24} {:>15}".format(self.name, self.address, self.phone, self.mail, self.birthday)
+
+
+class Application(object):
+
+    def __init__(self, database):
+        self.database = database
+        self.persons = {}
+        if not os.path.exists(self.database):
+            file_pointer = open(self.database, 'wb')
+            pickle.dump({}, file_pointer)
+            file_pointer.close()
+        else:
+            with open(self.database, 'rb') as person_list:
+                self.persons = pickle.load(person_list)
+
+    def delete(self):
+        name = input("Enter the name to delete: ")
+        if name in self.persons:
+            del self.persons[name]
+            print("Deleted the contact.")
+        else:
+            print("Contact not found in the app.")
