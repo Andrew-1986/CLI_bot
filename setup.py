5810fa193b768ad91b72322ca78697f40ce1384c
Andrew-1986 <plaskalny@gmail.com>
2021-06-23 14:46:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='cli_bot',
-      version='1.2',
+      version='1.3',
       description='Console bot for contact book managing',
       url='https://github.com/Andrew-1986/CLI_bot',
       author='Group_4',
