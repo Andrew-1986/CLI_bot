79c26329294e5b1f46ff078ed2b343ac7d9764d4
Andrew-1986 <plaskalny@gmail.com>
2021-06-23 10:47:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
       name='cli_bot',
       version='1.0',
       description='Console bot for contact book managing',
-      url='https://github.com/47codemonkey/personal-assistant',
+      url='https://github.com/Andrew-1986/CLI_bot',
       author='Group_4',
       entry_points={'console_scripts': ['bot = main:main']},
       packages=['main']
